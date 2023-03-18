1ca0d38967756ac9ba83ecc60fa6b9b9dfe3bada
Harlan Ferguson <harlanjazz@hotmail.com>
2023-03-18 17:05:20 -0400

    finished assignment

--- functions.py
+++ functions.py
@@ -1,5 +1,10 @@
+#Harlan Ferguson 101133838
 #Collection of all functions required for this assignment
 import random 
+import time
+import math
+
+#-------------------------Random Equation Portion -----------------------------------
 
 def randomEquation():
     symbols = ("*","+","-")
@@ -22,8 +27,6 @@ def randomEquation():
         return answer
     else:
         print("Error. Please restart program")
-
-    
 
 def isEmpty(): #need to also check to see if file exsists
     with open("Score.txt", "rb") as file: #rb = read binary. File either empty with 0 bytes, or has something
@@ -49,8 +52,6 @@ def isFileOk():
     except Exception as e:
         print(f"Ran into an unexpected error: {e}")
 
-
-
 def getScore():
     with open("Score.txt","r") as file:
         content = file.read()
@@ -60,7 +61,7 @@ def newScore(score):
     with open("Score.txt", "w") as file:
         file.write(str(score)) #write argument must be str, not int
 
-def menu():
+def mathOne():
     currScore = 0
     isFileOk()
     if isEmpty():
@@ -81,6 +82,7 @@ def menu():
                 print("Thanks for playing!")
                 print(f"Your current score is {currScore}")
                 print("WOW!")
+                time.sleep(1)
                 break
             else:
                 continue
@@ -90,7 +92,73 @@ def menu():
             exit = input("Would you like to stop playing? y/n").lower()
             if (exit == "y"):
                 newScore(currScore)
+                print("Thanks for playing!")
+                print(f"Your current score is {currScore}")
+                print("WOW!")
+                time.sleep(1)
                 break
             else:
                 continue  
 
+#-------------------------Quadratic Portion -----------------------------------
+
+def discriminant(a, b, c): #used to find roots of quadratic formula
+    return b**2 - 4*a*c
+
+def quadratic(a, b, c):
+    discrim = discriminant(a, b, c)
+    
+    if discrim > 0: #if positive, there are two answers
+        x1 = (-b + math.sqrt(discrim)) / (2 * a)
+        x2 = (-b - math.sqrt(discrim)) / (2 * a)
+        return (2, x1, x2) #returns number of solutions and the 2 soltions as a tuple
+    elif discrim == 0:
+        x1 = -b / (2 * a)
+        return (1, x1) #returns 1 solution and the solution as tuple
+    else: #answer too complex becuase negative number
+        return (0,) #returns a 0 tuple (to work with if-else block later)
+
+
+
+def mathTwo():
+    print("Welcome to the quadratic game!")
+    print("aX^2 + bX + C = 0")
+    a = float(input("Please input value for a: "))
+    b = float(input("Please input value for b: "))
+    c = float(input("Please input value for c: "))
+    print(f"Your Quadratic Equation is: {a}X^2 + {b}X + {c} = 0")
+
+    answer = quadratic(a,b,c)
+    
+    with open("QuadEqu.txt", "w") as file:
+        file.write(f"Your Quadratic Equation is: {a}X^2 + {b}X + {c} = 0\n")
+        
+        if answer[0] == 2:
+            print(f"We got two real answers, which are {answer[1]} and {answer[2]}")
+            file.write(f"We got two real answers, which are {answer[1]} and {answer[2]}\n")
+        elif answer[0] == 1:
+            print(f"We got one real answer, which is {answer[1]}")
+            file.write(f"We got one real answer, which is {answer[1]}\n")
+        else:
+            print("The Discriminant is negative, we got a pair of Complex answers.")
+            file.write("The Discriminant is negative, we got a pair of Complex answers.\n")
+
+#--------------------Menu to control the games played----------------------------------
+
+def mainMenu():
+    while True:
+        print("Welcome to math games!")
+        print("Type 1 for +,-,* equation game")
+        print("Type 2 for quadratic game")
+        print("Type 3 to exit")
+        userIn = input()
+        if(userIn == "1"):
+            mathOne()
+        elif(userIn == "2"):
+            mathTwo()
+        elif(userIn == "3"):
+            break
+        else:
+            print("Please enter either 1 2 or 3")
+            time.sleep(1) #without a sleep, it's hard to see the input message
+            continue
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#Harlan Ferguson 101133838
 from functions import*
 if __name__ == "__main__":   
-   menu()
+   mainMenu()
